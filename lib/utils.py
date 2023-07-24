0a11bd6fac4bbdc07a291e1d268caeccde27e291
Andrew <andrewsmith1025@gmail.com>
2023-07-24 17:50:53 -0400

    work today at lab

--- lib/utils.py
+++ lib/utils.py
@@ -10,7 +10,92 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, TensorDataset
 import random
 import os
-
+from sklearn.metrics import f1_score,recall_score,precision_score
+from os import listdir
+from pandas import read_csv
+from json import load
+from torch import from_numpy,zeros,cat
+import torch
+from torch.utils.data import DataLoader,TensorDataset
+from torch import nn
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score,recall_score,precision_score,confusion_matrix
+from seaborn import heatmap
+from tqdm import tqdm
+DATA_DIR = f'../data/nursingv1/'
+LABEL_DIR = f'../data/nursingv1_andrew'
+def get_labeled_nursing_ids():
+    return sorted([int(file.split('_')[0]) for file in listdir(f'../data/nursing_andrew')])
+def load_feature_label_pair(index=0,data_dir=DATA_DIR,label_dir=LABEL_DIR):
+    X = load_features(index,data_dir)
+    y = load_labels(index,len(X),label_dir)
+    return X,y
+def load_features(index=0,data_dir=DATA_DIR):
+    return from_numpy(read_csv(f'{data_dir}/{index}/raw_data.csv',header=None)[[2,3,4]].to_numpy())
+def load_labels(index,length,label_dir=LABEL_DIR):
+    with open(f'{label_dir}/{index}_data.json','r') as f:
+        data = load(f)
+    y = zeros(length)
+    for puff in data['puffs']:
+        y[puff['start']:puff['end']] = 1
+    y = y.unsqueeze(1).float()
+    return y
+def window_epoched_signal(X,windowsize,zero_padding=True):
+    """
+    only works for odd windows, puts label at center
+    """
+    if(zero_padding):
+        X = torch.cat([torch.zeros(windowsize//2,3),X,torch.zeros(windowsize//2,3)])
+    cat = [X[:-(windowsize-1)]]
+    for i in range(1,(windowsize-1)):
+        cat.append(X[i:i-(windowsize-1)])
+    cat.append(X[(windowsize-1):])
+    cat = [xi.unsqueeze(2) for xi in cat]
+    X = torch.cat(cat,axis=2).float()
+    return X
+def cm_grid(y_true,y_pred,save_path='cm.jpg'):
+    fig,axes = plt.subplots(2,2,figsize=(5,5))
+    heatmap(confusion_matrix(y_true=y_true,y_pred=y_pred,normalize='true'),annot=True,fmt='.2f',cbar=False,ax=axes[0][0])
+    heatmap(confusion_matrix(y_true=y_true,y_pred=y_pred,normalize='pred'),annot=True,fmt='.2f',cbar=False,ax=axes[0][1])
+    heatmap(confusion_matrix(y_true=y_true,y_pred=y_pred,normalize='all'),annot=True,fmt='.2f',cbar=False,ax=axes[1][0])
+    heatmap(confusion_matrix(y_true=y_true,y_pred=y_pred),annot=True,fmt='.0f',cbar=False,ax=axes[1][1])
+    axes[0][0].set_title('Recall')
+    axes[0][1].set_title('Precision')
+    axes[1][0].set_title('Proportion')
+    axes[1][1].set_title('Count')
+    axes[0][0].set_xticks([])
+    axes[0][1].set_xticks([])
+    axes[0][1].set_yticks([])
+    axes[1][1].set_yticks([])
+    # axes[0][0].set_yticklabels(['P','S','W'])
+    # axes[1][0].set_yticklabels(['P','S','W'])
+    # axes[1][0].set_xticklabels(['P','S','W'])
+    # axes[1][1].set_xticklabels(['P','S','W'])
+    plt.savefig(save_path,dpi=200,bbox_inches='tight')
+    
+def metrics(y_true,y_pred):
+    return {
+        'precision':precision_score(y_true=y_true,y_pred=y_pred,average='macro'),
+        'recall':recall_score(y_true=y_true,y_pred=y_pred,average='macro'),
+        'f1':f1_score(y_true=y_true,y_pred=y_pred,average='macro')
+    }
+def evaluate(dataloader,model,criterion):
+    model.eval()
+    with torch.no_grad():
+        y_true = torch.Tensor()
+        y_pred = torch.Tensor()
+        y_logits = torch.Tensor()
+        loss_total = 0
+        for (Xi,yi) in tqdm(dataloader):
+            y_true = torch.cat([y_true,yi.round()])
+            logits = model(Xi)
+            loss = criterion(logits,yi)
+            loss_total += loss.item()
+            
+            y_logits = torch.cat([y_logits,torch.sigmoid(logits).detach().cpu()])
+            y_pred = torch.cat([y_pred,torch.sigmoid(logits).round().detach().cpu()])
+    model.train()
+    return loss_total/len(dataloader),metrics(y_true,y_pred),y_true,y_pred,y_logits
 def load_nursing_by_index(index,data_dir='../data/nursingv1/',label_dir='../data/nursingv1_andrew'):
     i = index
     df = pd.read_csv(f'{data_dir}/{i}/raw_data.csv',header=None)
